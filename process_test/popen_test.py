75cbc1be2014e2097cb7d93599891519b7514aae
zifenghello <zifeng.w@foxmail.com>
2017-05-21 23:40:00 +0800

    mkdir process_test and thread_test

---
+++ process_test/popen_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+def popen_pipe():
+    child1 = subprocess.Popen(['ls', '-l'], stdout=subprocess.PIPE)
+    child2 = subprocess.Popen(['wc'], stdin=child1.stdout, stdout=subprocess.PIPE)
+    out, err = child2.communicate() # Func_communicate returns the tuple (out, err)
+    print(out.decode())
+
+def popen_input():
+    child = subprocess.Popen(['mysql','-uroot','-pphoenix'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, err = child.communicate(input=b'use test;\n show tables;\nexit\n')
+    print(out.decode())
+
+if __name__ == '__main__':
+    #popen_pipe()
+    popen_input()
+
